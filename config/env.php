b70af470e8515dff15e6276b85cde2a082f350ea
George Lemon <things@wemake.co.de>
2020-04-10 14:11:00 +0300

    renaming /backdoor admin to /backend

--- config/env.php
+++ config/env.php
@@ -1,45 +1,152 @@
 <?php
+
 /**
   *   Charti CMS
   *   
   *   @since 0.1
   *
-  **/ 
-  
-  /*
-    This will be useful in future compatibility
-    for Plugins & Themes between Chartí CMS  and Wordpress
+  *
+  *  This will be useful in future compatibility
+  *  for Plugins & Themes between Chartí CMS and Wordpress
   */
   define( 'IS_CHARTI', true ); // this should stay true
 
-  /*
-    CMS Core
-    All directories are located outside of the public directory
-      
-      ROOT_PATH - Points to the root of the project
-      ROUTES_PATH - Points to the Routing directory
-      WPINC - Points to the WP & Charti Core directory
-      CONFIG_DIR - Points to configuration directory
-  */
-  define( 'ROOTPATH', ABSPATH . '../' );
+  
 
-  define( 'ROUTESPATH', ABSPATH . '../routes/' );
+  /**
+   * ROOT_PATH directory
+   * 
+   * Found: in root
+   * Editable: Yes (For subdirectory installs)
+   * Default '../'
+   * 
+   * @since Charti CMS 1.0
+  */
+  define( 'ROOT_PATH', ABSPATH . '../' );
+  
+
+  /**
+   * ROUTES_PATH directory
+   * 
+   * Found: in root
+   * Editable: Yes (For subdirectory installs)
+   * Default '../'
+   * 
+   * @since Charti CMS 1.0
+  */
+  define( 'ROUTES_PATH', ABSPATH . '../routes/' );
 
   define( 'WPINC', '../app' );
 
-  define( 'CONFIG_DIR', ROOTPATH . 'config/' );
+  define( 'CONFIG_DIR', ROOT_PATH . 'config/' );
   
-  // CMS Admin
-  define( 'ADMIN_ROOT', 'backdoor' ); // Points to private admin directory
 
-  define( 'ADMIN_DIR', 'backdoor' ); // The ex. wp-admin, which is located in public/admin
+  /**
+   * ADMIN_DIR directory
+   * 
+   * Found: in public/ADMIN_DIR
+   * Editable: Yes (Requires Renaming Directory)
+   * Default 'backend'
+   * 
+   * @since Charti CMS 1.0
+  */
+  define( 'ADMIN_ROOT', 'backend' ); // Points to private admin directory
+
+
+  /**
+   * ADMIN_DIR directory
+   * 
+   * Found: in public/ADMIN_DIR
+   * Editable: Yes (Requires Renaming Directory)
+   * Default 'backend'
+   * 
+   * @since Charti CMS 1.0
+  */
+  define( 'ADMIN_DIR', 'backend' );
   
+
+  /**
+   * ADMIN_ASSETS directory
+   * 
+   * Found: in public/backend/ADMIN_ASSETS
+   * Editable: Yes (Requires Renaming Directory)
+   * Default '/assets/admin'
+   * 
+   * @since Charti CMS 1.0
+  */
   define( 'ADMIN_ASSETS', ADMIN_DIR . '/assets/admin' ); // 
-  // Media, Plugin & Themes
-  define( 'WP_CONTENT_DIR', 'resources' ); // Ex wp-content
-  define( 'UPLOADS', 'media' );
-  // define( 'WP_PLUGIN_DIR', ABSPATH . '../resources/plugins' );
-  define( 'CHARTI_CONFIGURATIONS__DIR', WP_CONTENT_DIR . '/configurations' );
+  
 
-  // Development 
+  /**
+   * WP_CONTENT_DIR directory
+   * 
+   * Found: in public/WP_CONTENT_DIR
+   * Editable: Yes (Requires Renaming Directory)
+   * Default: "resources"
+   *
+   * @since Wordpress
+   * Fully integrated @since Charti CMS 1.0
+  */
+  define( 'WP_CONTENT_DIR', 'resources' ); // Ex wp-content
+  
+
+  /**
+   * UPLOADS directory
+   * 
+   * Found: in public/* directory
+   * Editable: Yes (Requires Renaming Directory)
+   * Default: 'media' 
+   *
+   * @since Wordpress
+   * Fully integrated @since Charti CMS 1.0
+  */
+  define( 'UPLOADS', 'media' );
+
+
+  /**
+   * WP_PLUGIN_DIR directory
+   * 
+   * Found: in public/* directory
+   * Editable: Yes (Requires Renaming Directory)
+   * Default: 'plugins' 
+   *
+   * @since Wordpress
+   * Fully integrated @since Charti CMS 1.0
+  */
+  
+  //define( 'WP_PLUGIN_DIR', ABSPATH . '../resources/plugins' );
+
+
+  /**
+   * CONFIGURATION_DIR directory
+   * 
+   * Found in root project
+   * Editable: Yes (Requires Renaming Directory)
+   * Default: 'config' 
+   *
+   * @since Charti CMS 1.0
+  */
+  define( 'CONFIGURATION_DIR', WP_CONTENT_DIR . '/configurations' );
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+  define( 'WP_DEBUG', true );
+
+  define( 'WP_DEBUG_DISPLAY', true );
+
+  // Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
   define( 'SCRIPT_DEBUG', true );
+
+  define( 'SHORTINIT', false);
+  
+  define( 'RESTSPLAIN_DEBUG', true );
